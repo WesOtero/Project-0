13b4cc5970ca42dc46aceb4457e35cc33d140444
Wes Otero <wesleyotero@protonmail.com>
2020-01-12 10:15:38 -0500

    CarBidService add and remove completed

--- CarDealershipApp/src/main/java/com/revature/service/CarBidService.java
+++ CarDealershipApp/src/main/java/com/revature/service/CarBidService.java
@@ -5,10 +5,17 @@ import com.revature.pojo.Car;
 
 public class CarBidService {
 	//User doesn't need auth because they're already past the login screen
-	public void customerOffer(String carVin, Double offer) {
-		if (LotDAO.getLot().containsKey(carVin)) {
-			Car car = LotDAO.getLot().get(carVin);
-//			car.setOffers(username, offer);
+	public void addOffer(String vinNumber, String username, Double offer) {
+		if (LotDAO.getLot().containsKey(vinNumber)) {
+			Car car = LotDAO.getLot().get(vinNumber);
+			car.setOffers(username, offer);
+		}
+	}
+	
+	public void removeOffer(String custUsername, String vinNumber) {
+		if (LotDAO.getLot().containsKey(vinNumber)) {
+			Car car = LotDAO.getLot().get(vinNumber);
+			car.getOffers().remove(custUsername);
 		}
 	}
 }
